c1168e2fd147a2db34ba9d61586b7af9844f985c
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-12-19 04:23:50 +0100

    update

--- timelapse/record-frames_jpeg-src.sh
+++ timelapse/record-frames_jpeg-src.sh
@@ -5,7 +5,7 @@ GST_LAUNCH=gst-launch-1.0
 
 export leddev=/dev/ttyUSB0
 export v4ldev=( /dev/video?([-1]) )
-local intervall=20
+local intervall=30
 
 
 local -i imgnum=0
@@ -36,28 +36,40 @@ function takePicture {
   local outfilename="${dir}/frame_$(print -f "%06d" imgnum).jpg"
   echo -n 1 > $leddev
   sleep 5 # give cam time to adjust
-  $GST_LAUNCH  v4l2src device=$v4ldev num-buffers=40 !  textoverlay text="$((imgnum*intervall))s" line-alignment=0 halignment=2 ! jpegenc ! image/jpeg,width={ 1920, 1280, 1024, 864, 640 },height={ 1080, 768, 720, 480 },pixel-aspect-ratio=1/1 ! filesink location="$outfilename"
+  $GST_LAUNCH  v4l2src device=$v4ldev num-buffers=40 ! jpegenc ! image/jpeg,width={ 1920, 1280, 1024, 864, 640 },height={ 1080, 768, 720, 480 },pixel-aspect-ratio=1/1 ! filesink location="$outfilename"
+  ##with text before distort
+  #$GST_LAUNCH  v4l2src device=$v4ldev num-buffers=40 !  textoverlay text="$((imgnum*intervall))s" line-alignment=0 halignment=2 ! jpegenc ! image/jpeg,width={ 1920, 1280, 1024, 864, 640 },height={ 1080, 768, 720, 480 },pixel-aspect-ratio=1/1 ! filesink location="$outfilename"
   echo -n 0 > $leddev
   ## Creative Live Camera, 640x480
   #mogrify -distort Perspective '40,36 0,0 680,50 640,0 141,401 0,480 556,409 640,480 ' "$outfilename"
-  ## Microsoft Camera 1280x720
-  mogrify -distort Perspective '330,0 0,0 1480,0 1080,0 0,1070 0,1080, 1950,1070 1080,1080' -crop 1080x1080+0+0 "$outfilename"
+
+  ## Microsoft Camera 1920x1080 for PetriDish
+  #mogrify -distort Perspective '330,0 0,0 1480,0 1080,0 0,1070 0,1080, 1950,1070 1080,1080' -crop 1080x1080+0+0 "$outfilename"
+
+  ## Microsoft Camera 1920x1080 for Labyrinth
+  local imgwidth=1240
+  local imgheight=720
+  mogrify -distort Perspective "250,80 0,0 1520,74 ${imgwidth},0 123,954 0,${imgheight}, 1640,956 ${imgwidth},${imgheight}" -crop "${imgwidth}x${imgheight}+0+0" "$outfilename"
+  mogrify -pointsize 50 -fill orange -undercolor '#00000080' -gravity SouthEast -annotate +0+0 "$((imgnum*intervall))s"  "$outfilename"
 }
 
 
 #$GST_LAUNCH  v4l2src device=/dev/video0 num-buffers=1 ! image/jpeg,width={ 1280, 864, 640 },height={ 720, 480 },framerate={ 25/1, 24/1, 30/1 },pixel-aspect-ratio=1/1 ! jpegdec ! jpegenc ! filesink location="$outfilename"
 
-local LASTPID
-while sleep $intervall; do
-  [[ -n $LASTPID ]] && wait $LASTPID
-  takePicture $outdir $imgnum &
-  LASTPID=$!
-  echo $((imgnum++))
-done
+{
+  local LASTPID
+  while sleep $intervall; do
+    [[ -n $LASTPID ]] && wait $LASTPID
+    takePicture $outdir $imgnum &
+    LASTPID=$!
+    echo $((imgnum++))
+  done 
+} always {
+  echo EXITING....
+  echo
+  kill -9 $CATPID
+  wait $LASTPID
+  echo -n 0 > $leddev
+  exit 0
+}
 
-echo 
-echo EXITING....
-echo
-wait $LASTPID
-exit 0
-#convert "$tmpfile" -crop "1080x1080+0+0"  "$outfilename"
